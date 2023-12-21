b25f0f9f066c0f7368082abd01456ccf388eb19c
Dermie Madsen <53880481+dermie83@users.noreply.github.com>
2023-12-21 15:44:56 +0000

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -13,7 +13,7 @@
 #define WIFI_PASSWORD "" // replace YourPassword with your WiFi password
 
 // Openweather API
-#define SECRET_APPID "76526af6b8849193d9b8d3593c49421d"
+#define SECRET_APPID ""
 #define SECRET_LOC "Dublin, IE" // use this if using free api calls
 // use lat, lon for exact location if signed up with OneCall on openweather app
 #define SECRET_LAT "53.3610784" 
